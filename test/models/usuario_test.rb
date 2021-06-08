4cfe94b22ec289e890dd09faaaff179ba408f0f9
Felix Cuello <felix.cuello@gmail.com>
2021-06-08 18:31:43 -0300

    Un test sencillo para el usuario

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class UsuarioTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  @@user_name = 'felix_test'
+  @@user_pass = 'felix_test'
+  @@user_invalid_pass = 'xxxxxxx'
+
+  test 'valid user' do
+    usuario = Usuario.find_by(user: @@user_name)
+    usuario = Usuario.new(nombre: 'felix', user: @@user_name) if usuario.nil?
+
+    usuario.password = ::V1::Password.sha512(string: @@user_pass)
+    usuario.save!
+
+    assert_not_nil(::Credentials.check_username_password(user: @@user_name, password: @@user_pass))
+  end
+
+
+  test 'invalid user' do
+    usuario = Usuario.find_by(user: @@user_name)
+    usuario = Usuario.new(nombre: 'felix', user: @@user_name) if usuario.nil?
+
+    usuario.password = ::V1::Password.sha512(string: @@user_pass)
+    usuario.save!
+
+    begin
+      ::Credentials.check_username_password(user: @@user_name, password: @@user_invalid_pass)
+      assert(false) # ESTO TIENE que fallar en caso de excepción
+    rescue Exceptions::UserNotFound
+      assert(true)  # Si lanza excepción está bien
+    end
+  end
 end
